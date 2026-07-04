2be4150eb5492dd1dbe366933517015ae2dbc699
thiagoRamos26 <devisonthiago26@gmail.com>
2026-07-04 17:10:35 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function green() {
+    document.body.style.backgroundImage = "url('/foryouvs/img/fcv.jpeg')";
+    document.getElementById("uf").innerHTML = "Tenho certeza de que seremos felizes!"; 
+    document.getElementById("uf").style.color = "green";
+    document.getElementById("p").innerHTML = "(Manda print)";
+}
+
+function red() {
+    document.body.style.backgroundImage = "url('/foryouvs/img/fcr.jpeg')";
+    document.getElementById("uf").innerHTML = "Que pena..."; 
+    document.getElementById("uf").style.color = "white";
+    document.getElementById("p").innerHTML = "(Manda print)";
+}
